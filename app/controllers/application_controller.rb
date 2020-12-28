23112e796aac2fe3614d7ae69dcfc7e7fffe0d40
Sean <skabe89@gmail.com>
2020-12-28 16:16:24 -0500

    commit tree 6274094a1d803d257f97735eaa221fb0d995ecf9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,18 +20,18 @@ class ApplicationController < Sinatra::Base
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     session[:user_id] = @user.id
-
+    
     redirect '/users/home'
   end
 
   get '/sessions/login' do
-
     # the line of code below render the view page in app/views/sessions/login.erb
     erb :'sessions/login'
   end
 
   post '/sessions' do
     @user = User.find_by(email: params[:email], password: params[:password])
+    # binding.pry
     if @user
       session[:user_id] = @user.id
       redirect '/users/home'
@@ -45,7 +45,6 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-
     @user = User.find(session[:user_id])
     erb :'/users/home'
   end
